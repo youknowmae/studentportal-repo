775a70451039f6cab0c479bddeb270edc087a8ae
ryanentrolezo030 <ryanentrolezo@gmail.com>
2024-04-18 00:50:13 +0800

    updated

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './main/main.component';
 import { MainModule } from './main/main.module';
 import { FormsModule } from '@angular/forms';
+import { HistoryComponent } from './history/history.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
@@ -27,4 +29,5 @@ import { FormsModule } from '@angular/forms';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
