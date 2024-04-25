0ba7be04093ed6fe7fbf48935716744f34f0ee30
youknowmae <ganaangelmae@gmail.com>
2024-04-25 14:14:58 +0800

    dashboard

--- src/app/main/components/dashboard/dashboard-routing.module.ts
+++ src/app/main/components/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,7 @@ import { ThesisComponent } from './components/thesis/thesis.component';
 import { CapstoneComponent } from './components/capstone/capstone.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'thesis', pathMatch: 'full' },
   { path: 'thesis', component: ThesisComponent },
   { path: 'capstone', component: CapstoneComponent }];
 
