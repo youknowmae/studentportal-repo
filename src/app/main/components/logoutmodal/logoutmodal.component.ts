9eb74a816902556efd5926c068b0fd177d5d9ad8
ryanentrolezo030 <ryanentrolezo@gmail.com>
2024-05-12 19:48:39 +0800

    updated

--- src/app/main/components/logoutmodal/logoutmodal.component.ts
+++ src/app/main/components/logoutmodal/logoutmodal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-logoutmodal',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './logoutmodal.component.scss'
 })
 export class LogoutmodalComponent {
+  @Input() isVisible: boolean = false;
+  @Input() selectedProject: any;
 
+  closeModal(): void {
+    this.isVisible = false;
+}
 }
