86cfd015d216ae86f5a396081eab5dde9665d25b
youknowmae <ganaangelmae@gmail.com>
2024-05-28 21:02:15 +0800

    updated borrowed

--- src/app/main/components/modal/qrmodal/qrmodal.component.ts
+++ src/app/main/components/modal/qrmodal/qrmodal.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-qrmodal',
   templateUrl: './qrmodal.component.html',
   styleUrls: ['./qrmodal.component.scss']
 })
 export class QrmodalComponent {
-  showModal = true;
-
-
-  closeModal(): void {
-    this.showModal = false;
-  }
+  showModal: boolean = false;
 }
