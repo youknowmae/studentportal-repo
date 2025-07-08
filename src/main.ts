80b1c1acdedefcc4115928d4e8c27830c2557244
youknowmae <ganaangelmae@gmail.com>
2025-07-08 08:49:23 +0800

    pushed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  window.console.log = ()=>{ }
+  window.console.warn = ()=>{ }
+  window.console.error = ()=>{ }
+  enableProdMode();
+}
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
